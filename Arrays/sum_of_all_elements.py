87a6f79b6f2ecea161bd50d1d5771a0da766be1d
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-01-24 10:51:04 +0300

    Add algorithm to calculate the sum of all elements in a list

---
+++ Arrays/sum_of_all_elements.py
@@ -0,0 +1,7 @@
+#algorithim to find the sum of all elements in an array
+
+numbers = [10, 20, 30, 40, 50]
+sum_of_elements = 0
+for number in numbers:
+    sum_of_elements += number
+print("The sum of all elements is:", sum_of_elements)
