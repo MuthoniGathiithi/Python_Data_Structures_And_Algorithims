1d0fd27ff4782a12eb6378db02c001c1d102b487
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-01-29 10:54:17 +0300

    add Function to calculate the sum of two numbers

---
+++ Functions/add_two_numbers.py
@@ -0,0 +1,11 @@
+# Function to calculate the sum of two numbers
+
+
+
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+print("The sum is:", add_numbers(num1, num2))   
