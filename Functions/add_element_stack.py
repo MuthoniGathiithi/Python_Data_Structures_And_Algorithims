e39d3e3cd291bed5cf2a7379b7ac2e09586beeee
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-01-29 10:51:09 +0300

    Add function to sum two numbers

---
+++ Functions/add_element_stack.py
@@ -0,0 +1,7 @@
+list = [303,50.89,79]
+
+def add_element_stack(list):
+    element = input("Enter an element to add to the stack: ")
+    list.append(element)
+    return list
+print("Updated stack:", add_element_stack(list))
